d816a19058d3bb7b9fe6aefc2d3cad7de911b340
Fred Mohier <frederic/mohier@gmail.com>
2019-03-24 06:02:47 +0100

    French translation

--- tools/update_template.sh
+++ tools/update_template.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 # Create the pot file
-xgettext *.php */*.php -o locales/monitoring.pot -L PHP --add-comments=TRANS --from-code=UTF-8 --force-po \
+xgettext *.php ajax/*.php front/*.php inc/*.php -o locales/monitoring.pot -L PHP --add-comments=TRANS --from-code=UTF-8 --force-po \
     --keyword=_n:1,2 --keyword=__s --keyword=__ --keyword=_e --keyword=_x:1c,2 --keyword=_ex:1c,2 --keyword=_sx:1c,2 --keyword=_nx:1c,2,3 --keyword=_sn:1,2
 
 # Update French language
 msgmerge --update locales/fr_FR.po locales/monitoring.pot
+# Update English language
+msgmerge --update locales/en_GB.po locales/monitoring.pot
